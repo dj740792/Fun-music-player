10d89f7e1e61b2230adf29c716048aeb58301c15
Dhruv <dj740792@gmail.com>
2026-01-04 19:31:40 +0530

    first commit

---
+++ Music player/script.js
@@ -0,0 +1,50 @@
+const songs = [
+  {
+    image: "./jeffbuckley.jpg",
+    name: "Lover you should've come over",
+    artist: "Jeff Buckley",
+    audio: "./jeffsong.mp3",
+  },
+  {
+    image: "./deftones.jpg",
+    name: "Sextape",
+    artist: "Deftones",
+    audio: "./sextape.mp3",
+  },
+  {
+    image: "./franksinatra.jpg",
+    name: "Stranger in the night",
+    artist: "Frank Sinatra",
+    audio: "./frank.mp3",
+  },
+  {
+    image: "./bemybaby.jpg",
+    name: "Be my baby",
+    artist: "The Ronettes",
+    audio: "./bemybaby.mp3",
+  },
+  {
+    image: "./radiohead.jpg",
+    name: "All i need",
+    artist: "RadioHead",
+    audio: "./allineed.mp3",
+  },
+  {
+    image: "./sade.jpg",
+    name: "Kiss of life",
+    artist: "Sade",
+    audio: "./sade.mp3",
+  },
+  {
+    image: "./wham.jpg",
+    name: "Last christmas",
+    artist: "Wham",
+    audio: "./wham.mp3",
+  },
+  {
+    image: "./johnlennon.jpg",
+    name: "Stand by me",
+    artist: "John Lennon",
+    audio: "./lennon.mp3",
+  },
+];
